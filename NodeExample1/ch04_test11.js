48b2c706aaff0f2db30456bcc2ef45f7146fdb6a
yeLyn36 <s2018w11@e-mirim.hs.kr>
2020-01-08 15:50:45 +0900

    use Stream

---
+++ NodeExample1/ch04_test11.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+
+var infile = fs.createReadStream('./output.txt', { flags: 'r' });
+var outfile = fs.createWriteStream('./output2.txt', { flags: 'w' });
+
+infile.on('data', function(data) {
+  console.log('읽어들인 데이터', data);
+  outfile.write(data);
+});
+
+infile.on('end', function() {
+  console.log('읽기 완료');
+  outfile.end(function() {
+    console.log('파일 쓰기 종료');
+  });
+});
