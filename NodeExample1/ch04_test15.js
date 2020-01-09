54e016291a074665ea05e2774eb68f6c7d690dcc
yeLyn36 <s2018w11@e-mirim.hs.kr>
2020-01-09 10:50:26 +0900

    create log file

---
+++ NodeExample1/ch04_test15.js
@@ -0,0 +1,65 @@
+var winston = require('winston'); // 로그 처리 모듈
+var winstonDaily = require('winston-daily-rotate-file');
+// 로그 일별 처리 모듈
+var moment = require('moment'); // 시간 처리 모듈
+function timeStampFormat() {
+  return moment().format('YYYY-MM-DD HH:mm:ss.SSS ZZ');
+  // ' :14: '
+}
+var logger = winston.createLogger({
+  //version update로 인해 new winston.Logger 사용 불가
+  transports: [
+    new winstonDaily({
+      name: 'info-file',
+      filename: './log/server',
+      datePattern: '_YYYY-MM-dd.log',
+      colorize: false,
+      maxsize: 50000000,
+      maxFiles: 1000,
+      level: 'info',
+      showLevel: true,
+      json: false,
+      timestamp: timeStampFormat
+    }),
+    new winston.transports.Console({
+      name: 'debug-console',
+      colorize: true,
+      level: 'debug'
+    })
+  ],
+  exceptionHandlers: [
+    new winstonDaily({
+      name: 'exception-file',
+      filename: './log/exception',
+      datePattern: '_YYYY-MM-dd.log',
+      colorize: false,
+      maxsize: 50000000,
+      maxFiles: 1000,
+      level: 'error',
+      showLevel: true,
+      json: false,
+      timestamp: timeStampFormat
+    }),
+    new winston.transports.Console({
+      name: 'exception-console',
+      colorize: true,
+      level: 'debug'
+    })
+  ]
+});
+
+var fs = require('fs');
+var inname = './output.txt';
+var outname = './output2.txt';
+fs.exists(outname, function(exists) {
+  if (exists) {
+    fs.unlink(outname, function(err) {
+      if (err) throw err;
+      logger.info('기존 파일 [' + outname + '] 삭제함.');
+    });
+  }
+  var infile = fs.createReadStream(inname, { flags: 'r' });
+  var outfile = fs.createWriteStream(outname, { flags: 'w' });
+  infile.pipe(outfile);
+  logger.info('파일 복사 [' + inname + '] -> [' + outname + ']');
+});
