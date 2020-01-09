7ec7320c1578fe91d2091100072a9e3f39bbe6a7
yeLyn36 <s2018w11@e-mirim.hs.kr>
2020-01-09 10:50:37 +0900

    modified

--- NodeExample1/ch04_test8.js
+++ NodeExample1/ch04_test8.js
@@ -7,7 +7,7 @@ fs.open('./output.txt', 'w', function(err, fd) {
   fs.write(fd, buf, 0, buf.length, null, function(err, written, buffer) {
     if (err) throw err;
 
-      console.log(err, written, buffer);
+    console.log(err, written, buffer);
 
     fs.close(fd, function() {
       console.log('파일 열고 데이터 쓰고 파일 닫기 완료');
