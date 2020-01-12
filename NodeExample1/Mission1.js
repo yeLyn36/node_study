124b496ae27e779c69da2933b840e096a4ad78f4
yeLyn36 <s2018w11@e-mirim.hs.kr>
2020-01-12 23:09:14 +0900

    complete mission1

--- NodeExample1/Mission1.js
+++ NodeExample1/Mission1.js
@@ -1,7 +1,17 @@
 var fs = require('fs');
 
-fs.open('mission1.txt', 'r', function(err) {
+fs.open('./mission1.txt', 'r', function(err, fd) {
   if (err) throw err;
-  console.log('mission1.txt open');
+
+  var buffer = Buffer.alloc(100);
+  fs.read(fd, buffer, 0, buffer.length, null, function(err, bytesRead, buffer) {
+    if (err) throw err;
+
+    var inStr = buffer.toString('utf8', 0, bytesRead);
+    console.log(inStr);
+
+    fs.close(fd, function() {
+      console.log('file 읽기 완료');
+    });
+  });
 });
-fs.close();
