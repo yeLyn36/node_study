ba55ec57193a707bca77471d5a6af311b8a1d54d
yeLyn36 <s2018w11@e-mirim.hs.kr>
2020-01-14 01:57:44 +0900

    read file by buffer

--- ch05_webserver/test6.js
+++ ch05_webserver/test6.js
@@ -13,10 +13,34 @@ server.on('connection', function() {
 });
 
 server.on('request', function(req, res) {
-  console.log('클라리언트 요청이 들어왔습니다.');
+  console.log('클라이언트 요청이 들어왔습니다.');
 
-  let filename = 'house.png';
+  const filename = 'house.png';
   let infile = fs.createReadStream(filename, { flags: 'r' });
+  let filelength = 0;
+  let curlength = 0;
 
-  infile.pipe(res);
+  fs.stat(filename, function(err, stats) {
+    filelength = stats.size;
+  });
+
+  res.writeHead(200, { 'Content-Type': 'image/png' });
+
+  infile.on('readable', function() {
+    var chunk;
+    while (null !== (chunk = infile.read())) {
+      console.log('읽어들인 데이터 크기 : %d 바이트', chunk.length);
+      curlength += chunk.length;
+      res.write(chunk, 'utf8', function(err) {
+        console.log(
+          '파일 부분 쓰기 완료 : %d, 파일 크기 : %d',
+          curlength,
+          filelength
+        );
+        if (curlength >= filelength) {
+          res.end();
+        }
+      });
+    }
+  });
 });
