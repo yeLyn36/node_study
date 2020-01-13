c38d1ada03e2eb9642a1b3248e04b7ec29b02f16
yeLyn36 <s2018w11@e-mirim.hs.kr>
2020-01-13 23:53:57 +0900

    show image (pipe stream and res object)

---
+++ ch05_webserver/test6.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer();
+
+const port = 3000;
+server.listen(port, function() {
+  console.log('웹 서버가 시작되었습니다. : %d', port);
+});
+
+server.on('connection', function() {
+  console.log('클라이언트와 접속하였습니다.');
+});
+
+server.on('request', function(req, res) {
+  console.log('클라리언트 요청이 들어왔습니다.');
+
+  let filename = 'house.png';
+  let infile = fs.createReadStream(filename, { flags: 'r' });
+
+  infile.pipe(res);
+});
