70a0ccb8878161d1ac9a16a6c606746b44814343
yeLyn36 <s2018w11@e-mirim.hs.kr>
2020-01-17 23:45:04 +0900

    modified

--- ExpressExample/app6.js
+++ ExpressExample/app6.js
@@ -20,7 +20,7 @@ app.use(function(req, res, next) {
   console.log('두 번째 미들웨어에서 요청을 처리함');
 
   res.writeHead('200', { 'Content-Type': 'text/html;charset=utf8' });
-  res.end('<h1>익스프레스 서버에서 ' + req.user + '가 응답한 결과입니다.</h1>');
+  res.end('<h1>Express 서버에서 ' + req.user + '가 응답한 결과입니다.</h1>');
 });
 
 http.createServer(app).listen(3000, function() {
