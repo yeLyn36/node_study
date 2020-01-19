fc57045b67d2190623d22196642b37907c2e013d
yeLyn36 <s2018w11@e-mirim.hs.kr>
2020-01-19 21:22:49 +0900

    use middleware by body-parser

---
+++ ExpressExample/app7.js
@@ -0,0 +1,35 @@
+const express = require('express'),
+  http = require('http'),
+  path = require('path');
+
+const bodyParser = require('body-parser'),
+  static = require('server-static');
+
+const app = express();
+
+app.set(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  console.log('첫 번째 미들웨어에서 요청을 처리함');
+
+  var userAgent = req.header('User-Agent');
+  var paramName = req.query.name;
+
+  res.writeHead('200', { 'Content-Type': 'text/html;charset=utf8' });
+  res.write('<h1>Express 서버에서 응답한 결과입니다.</h1>');
+  res.write('<div><p>User-Agent : ' + userAgent + '</p></div>');
+  res.write('<div><p>Param-Name : ' + paramName + '</p></div>');
+  res.end();
+});
+
+app.use(function(req, res, next) {
+  console.log('두 번째 미들웨어에서 요청을 처리함');
+
+  res.writeHead('200', { 'Content-Type': 'text/html;charset=utf8' });
+  res.end('<h1>Express 서버에서 ' + req.user + '가 응답한 결과입니다.</h1>');
+});
+
+http.createServer(app).listen(3000, function() {
+  console.log('Express 서버가 3000번 포트에서 시작됨');
+});
